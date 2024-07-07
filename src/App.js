21f506b2ec4eacae3ec96a063f46b61acf1c662a
Apastar16 <aparna19srivastava@gmail.com>
2024-07-07 12:19:34 +0530

    relative path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
    
 
    <Routes>
-<Route exact path ="Apastar16.github.io/ShoppyKart/" element={<Store />}/>
+<Route path ="/" element={<Store />}/>
 <Route path="/success" element={<Success/>}/>
 <Route path="/cancel" element={<Cancel/>}/>
 
